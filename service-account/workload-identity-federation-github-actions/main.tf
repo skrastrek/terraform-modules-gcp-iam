7dcd97855e26252f26f733fa35f8dbb6fd848042
SebRamsland <mail@sebastianramsland.com>
2024-11-01 14:06:05 +0100

    Fix variable reference

--- service-account/workload-identity-federation-github-actions/main.tf
+++ service-account/workload-identity-federation-github-actions/main.tf
@@ -34,5 +34,5 @@ resource "google_project_iam_member" "role" {
 resource "google_service_account_iam_member" "github_repository_workload_identity_user" {
   service_account_id = google_service_account.this.id
   role               = "roles/iam.workloadIdentityUser"
-  member             = "principalSet://iam.googleapis.com/${data.google_iam_workload_identity_pool.github_actions.name}/attribute.repository/${var.github_repository}"
+  member             = "principalSet://iam.googleapis.com/${data.google_iam_workload_identity_pool.github_actions.name}/attribute.repository/${var.github_actions_repository}"
 }
