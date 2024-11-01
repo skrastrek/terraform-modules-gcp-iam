cda21f17b34b8cf2d26ae6fccfb77b0f3935913e
SebRamsland <mail@sebastianramsland.com>
2024-11-01 12:40:17 +0100

    Add useful outputs

--- service-account/workload-identity-federation-aws/outputs.tf
+++ service-account/workload-identity-federation-aws/outputs.tf
@@ -9,3 +9,11 @@ output "name" {
 output "email" {
   value = google_service_account.this.email
 }
+
+output "workload_identity_pool_name" {
+  value = data.google_iam_workload_identity_pool.aws.name
+}
+
+output "workload_identity_pool_provider_name" {
+  value = data.google_iam_workload_identity_pool_provider.aws.name
+}
--- service-account/workload-identity-federation-github-actions/outputs.tf
+++ service-account/workload-identity-federation-github-actions/outputs.tf
@@ -9,3 +9,11 @@ output "name" {
 output "email" {
   value = google_service_account.this.email
 }
+
+output "workload_identity_pool_name" {
+  value = data.google_iam_workload_identity_pool.github_actions.name
+}
+
+output "workload_identity_pool_provider_name" {
+  value = data.google_iam_workload_identity_pool_provider.github_actions.name
+}
