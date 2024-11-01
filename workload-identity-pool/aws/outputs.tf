59323b51f1a44f5d0cac02c8915418795c2a248b
SebRamsland <mail@sebastianramsland.com>
2024-11-01 15:38:10 +0100

    Fix outputs

--- workload-identity-pool/aws/outputs.tf
+++ workload-identity-pool/aws/outputs.tf
@@ -1,15 +1,7 @@
-output "id" {
-  value = google_iam_workload_identity_pool.this.id
+output "workload_identity_pool_id" {
+  value = google_iam_workload_identity_pool.this.workload_identity_pool_id
 }
 
-output "name" {
-  value = google_iam_workload_identity_pool.this.name
-}
-
-output "provider_id" {
-  value = google_iam_workload_identity_pool_provider.this.id
-}
-
-output "provider_name" {
-  value = google_iam_workload_identity_pool_provider.this.name
+output "workload_identity_pool_provider_id" {
+  value = google_iam_workload_identity_pool_provider.this.workload_identity_pool_provider_id
 }
